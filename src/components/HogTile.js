91e37bc1ca3c1753ecb75249a6f82d53a5dd4c88
abdimutalib <abdimutalibsaid@gmail.com>
2024-11-04 11:36:00 +0300

    first commit

---
+++ src/components/HogTile.js
@@ -0,0 +1,31 @@
+
+import React from "react";
+
+const HogTile = ({ hog, onSelect, onHide }) => {
+  return (
+    <article className="hog-tile" onClick={() => onSelect(hog)}>
+      <header>
+        <img src={hog.image} alt={hog.name} />
+        <h3>{hog.name}</h3>
+        <p>{hog.specialty}</p>
+      </header>
+      <section>
+        <p>Weight: {hog.weight} kg</p>
+        <p>Greased: {hog.greased ? "Yes" : "No"}</p>
+        <p>Highest Medal: {hog["highest medal achieved"]}</p>
+      </section>
+      <footer>
+        <button
+          onClick={(e) => {
+            e.stopPropagation();
+            onHide(hog);
+          }}
+        >
+          Hide
+        </button>
+      </footer>
+    </article>
+  );
+};
+
+export default HogTile;
